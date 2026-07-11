6614ca3ba7590a0b9e7b0e05f4f5f7a680c72b79
sbp828 <surisettybhavya828@gmail.com>
2026-07-11 10:19:51 +0000

    jenkinsfile

--- ci-cd/jenkins-agent.sh
+++ ci-cd/jenkins-agent.sh
@@ -1,1 +1,7 @@
 sudo dnf install java-21-amazon-corretto -y
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+
+sudo yum -y install terraform
